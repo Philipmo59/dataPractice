e862d501e2f1b9989193bc70a19d5dff96e16e0e
Philip <philipmo59@gmail.com>
2024-07-17 20:16:29 -0400

    added makeObject function

---
+++ format.js
@@ -0,0 +1,26 @@
+mapOfSamples = {
+    "poolTubeName1":{
+        sampleName: "poolTubeName1",
+       reporterList: {
+        INFA: {},
+        reporter2: {},
+        reporter3: {},
+        reporter4: {
+            values: [],
+            average: 1
+        },
+       }
+    },
+    "poolTubeName2":{
+        sampleName: "poolTubeName2",
+        reporterList: {
+        reporter1: [],
+        reporter2: [],
+        reporter3: [],
+        reporter4: [],
+       }
+
+    }
+}
+
+
---
+++ script.js
@@ -0,0 +1,68 @@
+/**
+ * @typedef {Object} Sample
+ * @property {string} type 
+ */
+
+function main(){    
+    document.getElementById("fileInput").addEventListener("input", parseFile)
+
+}
+
+/**
+ * @param {InputEvent & { target: HTMLInputElement}} event
+*/
+function parseFile(event) {
+    const file = event.target.files[0];
+    Papa.parse(file, {
+        complete: (results, file) => {
+            console.log(results)
+            const mapOfSamples = makeObject(results)
+            getAverage(mapOfSamples)
+    }})
+}
+
+function makeObject(results) {
+    const mapOfSamples = new Map()
+    for (let i = 0; i < results.data.length; i++) {
+        let selectedArray = results.data[i];
+        if(selectedArray.length >= 20 && !selectedArray.includes("Reporter")) {
+            if(mapOfSamples.has(selectedArray[3])){                                 //If it has Key
+                const sampleProperties = mapOfSamples.get(selectedArray[3])
+                if(!sampleProperties.reporterList.has(selectedArray[4])){                  //If it does not have the Reporter name as a value
+                    const reporterValueMap = new Map()
+                    reporterValueMap.set("values",[selectedArray[20]])
+                    sampleProperties.reporterList.set(selectedArray[4],reporterValueMap)          //Set the Reporter name as key, and the CT as the Value
+                }
+                else{                                                                //If it does have the Reporter name
+                    const reporterName = sampleProperties.reporterList.get(selectedArray[4]).get("values")
+                    reporterName.push(selectedArray[20])
+                }
+            
+            }
+            else{
+                const reporterMapList = new Map()
+                const reporterValueMap = new Map([["values",[selectedArray[20]]]])
+                reporterMapList.set(selectedArray[4],reporterValueMap)
+                const sample = {
+                    name: selectedArray[3],
+                    reporterList: reporterMapList
+                }
+                mapOfSamples.set(selectedArray[3],sample)
+            }
+        }
+        
+    }
+    console.log(mapOfSamples)
+    return mapOfSamples
+}
+
+function getAverage(mapOfSamples){
+    for(let sample of mapOfSamples.values()){
+        console.log(sample)
+    }
+}
+
+
+
+main()
+
