d0cc4ec3d99b95297f774c5f55359b45f8b48ae7
Philipmo59 <philipmo59@gmail.com>
2024-08-19 14:03:57 -0400

    added table

---
+++ getCSV.js
@@ -0,0 +1,8 @@
+export function getCSV(){
+    const button = document.getElementById("getCSV").addEventListener("click",makeCSV)
+
+}
+
+function makeCSV(){
+    console.log("hello button")
+}
--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 /**
  * @typedef {Object} Sample
- * @property {string} type 
+ * @property {string} name - The name of the sample
+ * @property {Map<string, Map<string, Map<string, number[]> >>} reporterList - The name of the sample
  */
+
+import { getCSV } from "./getCSV.js";
 
 function main(){    
     document.getElementById("fileInput").addEventListener("input", parseFile)
-
+    getCSV()
 }
 
 /**
@@ -15,9 +18,10 @@ function parseFile(event) {
     const file = event.target.files[0];
     Papa.parse(file, {
         complete: (results, file) => {
-            console.log(results)
             const mapOfSamples = makeObject(results)
             getAverage(mapOfSamples)
+            const container = document.getElementById("container")
+            createTable(container,mapOfSamples)
     }})
 }
 
@@ -34,7 +38,7 @@ function makeObject(results) {
             const sampleName = selectedArray[3]  
             const ctValue = parseFloat(selectedArray[12])   
             const reporterName = selectedArray[4]      
-
+            const average = []
             //If it has the sample name        
             if(samples.has(sampleName)){                                                  
                 const sample = samples.get(sampleName)
@@ -57,8 +61,10 @@ function makeObject(results) {
             
             }
             else{
-                const reporterMapList = new Map()
-                const reporterValueMap = new Map([["values",[ctValue]]])
+                const reporterMapList = new Map();
+                let reporterValueMap 
+                if(isNumber(ctValue)) reporterValueMap = new Map([["values",[ctValue]]])
+                else reporterValueMap = new Map([["values",[]]])
                 reporterMapList.set(reporterName,reporterValueMap)
                 const sample = {
                     name: sampleName,
@@ -69,8 +75,8 @@ function makeObject(results) {
         }
         
     }
-    console.log(samples)
-    return samples
+    const sortedSamples = new Map(Array.from(samples).sort((a,b) => a[0].localeCompare(b[0])))
+    return sortedSamples
 }
 
 /**
@@ -79,17 +85,82 @@ function makeObject(results) {
  * @returns {boolean}
  */
 function isNumber(ctValue){
-    if(ctValue === NaN) return false
+    if(Number.isNaN(ctValue)) return false
     else return true
 }
 
+/**
+ * @param {Map<string, Sample>} mapOfSamples 
+ * @returns {boolean}   
+ */
 function getAverage(mapOfSamples){
     for(let sample of mapOfSamples.values()){
-        console.log(sample)
+        for(let reporterValues of sample.reporterList.values()){
+            const ctValues = reporterValues.get("values")
+            if(ctValues.length === 0){
+                reporterValues.set("average","N/A")
+            }
+            else{
+                const initialValue = 0
+                const average = ctValues.reduce((accumulator, currentValue) => accumulator + currentValue,initialValue) / ctValues.length
+                reporterValues.set("average",average)}
+        }
     }
+    return true
 }
 
-dgxf
+/**
+ * 
+ * @param {HTMLDivElement} container 
+ * @param {Map<string,Sample>} mapOfSamples 
+ */
+function createTable(container,mapOfSamples){
+    const tbl = document.createElement("table")
+    const tblBody = document.createElement("tbody")
+    const headerRow = document.createElement("tr")
+
+  // Creating the header "Sample", and the 4 reporters   
+    let reporters;
+    for(let sample of mapOfSamples.values()){
+        reporters = Array.from(sample.reporterList.keys())
+        break
+    }
+    const headers = ["Sample",...reporters.sort()];
+    for(let header of headers){
+        td = document.createElement("td")
+        td.textContent = header
+        headerRow.appendChild(td)
+    }
+    tblBody.appendChild(headerRow)
+  // Getting the Average and rounding it by the hundreth place and placing it on table row
+    for(let sample of mapOfSamples.values()){    
+        row = document.createElement("tr") 
+        sampleNameTd = document.createElement("td")
+        sampleNameTd.textContent = sample.name
+        row.appendChild(sampleNameTd)
+
+        for(let [reporter,ctValue] of sample.reporterList){
+            let ctValueTd = document.createElement("td")
+            let reporterAverage = ctValue.get("average")
+            if(reporterAverage === "N/A"){
+                ctValueTd.textContent = "N/A"
+                row.appendChild(ctValueTd)
+            }
+            else{
+                ctValueTd.textContent = Math.round(reporterAverage * 100) / 100
+                row.appendChild(ctValueTd)
+            }
+        }
+        tblBody.appendChild(row)
+    }
+
+    tbl.appendChild(tblBody)
+
+
+    container.appendChild(tbl)
+}
+
+
 
 main()
 
